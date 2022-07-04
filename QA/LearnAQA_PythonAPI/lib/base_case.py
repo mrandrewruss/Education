ff3f606c597eecc4e821694024a5b2882c7b9823
andrusgithub <tester.forumgr@gmail.com>
2022-07-04 15:35:48 +0500

    LearnQA_commit: modified base_case.py

--- QA/LearnAQA_PythonAPI/lib/base_case.py
+++ QA/LearnAQA_PythonAPI/lib/base_case.py
@@ -1,13 +1,12 @@
 import json.decoder
-
 from requests import Response
 
-class BaceCase:
+class BaseCase:
     def get_cookie(self, response: Response, cookie_name):
         assert cookie_name in response.cookies, f"Cannot find cookie with name {cookie_name} in the last response"
         return response.cookies[cookie_name]
 
-    def get_headers(self, response: Response, headers_name):
+    def get_header(self, response: Response, headers_name):
         assert headers_name in response.headers, f"Cannot find header with name {headers_name} in the last response"
         return response.headers[headers_name]
 
