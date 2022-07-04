3c996d248ba69083e92087f3c5c7cf425a6393e1
andrusgithub <tester.forumgr@gmail.com>
2022-07-04 17:38:46 +0500

    LearnQA_commit: add test_create_user_successfully + assert_json_has_key + assert_status_code

--- QA/LearnAQA_PythonAPI/tests/test_user_register.py
+++ QA/LearnAQA_PythonAPI/tests/test_user_register.py
@@ -1,8 +1,30 @@
 import requests
 from QA.LearnAQA_PythonAPI.lib.base_case import BaseCase
 from QA.LearnAQA_PythonAPI.lib.assertions import Assertions
+from datetime import datetime
+
 
 class TestUserRegister(BaseCase):
+    def setup(self):
+        base_part = "learnqa"
+        domain = "example.com"
+        random_part = datetime.now().strftime("%m%d%Y%H%S")
+        self.email = f"{base_part}{domain}{random_part}"
+
+    def test_create_user_successfully(self):
+        data = {
+            'email': self.email,
+            'password': '1234',
+            'username': 'learnqa',
+            'firstName': 'learnqa',
+            'lastName': 'learnqa'
+        }
+
+        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+
+        Assertions.assert_status_code(response, 200)
+        Assertions.assert_json_has_key(response, "id")
+
     def test_create_user_with_existing_email(self):
         email = 'vinkotov@example.com'
         data = {
@@ -10,10 +32,11 @@ class TestUserRegister(BaseCase):
             'password': '1234',
             'username': 'learnqa',
             'firstName': 'learnqa',
-            'lastName': 'learnqa',
+            'lastName': 'learnqa'
         }
 
         response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
 
-        assert response.status_code == 400, f"Unexpected status code {response.status_code}"
-        assert response.content.decode("utf-8") == f"Users with email '{email}' already exests", f"Unexpected response content {response.content}"
+        Assertions.assert_status_code(response, 400)
+        assert response.content.decode(
+            "utf-8") == f"Users with email '{email}' already exists", f"Unexpected response content {response.content}"
